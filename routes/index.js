021daf0a8fe9a1ba6d37b4fcf9d6b6cca3bd1576
Justin Reese <justin.x.reese@gmail.com>
2013-03-29 16:22:22 -0400

    Adds calculate feature

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,9 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.calculate = function(req, res){
+  var a = Number(req.query.a);
+  var b = Number(req.query.b);
+  res.send(JSON.stringify({result: a + b}));
+};
